dd74e3b28d1a13bc4bdb52477c584881a8b7267d
ronni söderberg <ronni.soderberg@gmail.com>
2022-12-28 21:51:36 +0100

    vad tycker alla?

--- Strapi/src/api/medlemskap/routes/medlemskap.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * medlemskap router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::medlemskap.medlemskap');
--- Strapi/src/api/medlemskap/services/medlemskap.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * medlemskap service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::medlemskap.medlemskap');
