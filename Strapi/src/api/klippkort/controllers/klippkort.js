874d0f39c2dd92d3a14c0b697edf21c1da6f30c3
ronni söderberg <ronni.soderberg@gmail.com>
2022-12-29 12:20:31 +0100

    delete and added back/frontend

--- Strapi/src/api/klippkort/controllers/klippkort.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * klippkort controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::klippkort.klippkort');
--- Strapi/src/api/klippkort/services/klippkort.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * klippkort service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::klippkort.klippkort');
