fa6b42d80a8d51a50871826c9dde8c7a60b96f1b
Pingu1337 <herman.brunbergg@gmail.com>
2023-01-08 00:47:37 +0100

    added roles with permissions

--- Strapi/frontend/Services/AuthService.cs
+++ Strapi/frontend/Services/AuthService.cs
@@ -1,5 +1,12 @@
 ﻿using frontend.Models;
+using System.Net.NetworkInformation;
+using System;
 using System.Text.Json;
+using static frontend.Pages.Index;
+using static System.Net.WebRequestMethods;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Json;
+using System.Net.Http.Headers;
 
 namespace frontend.Services
 {
@@ -9,13 +16,21 @@ namespace frontend.Services
         Task<string> GetUsernameAsync();
         Task<User> GetUserAsync();
         Task<string> GetJwt();
+        Task<int> GetRole(string Token);
+        Task<bool> IsElevated(int role);
     }
     public class AuthService : IAuthService
     {
         private ILocalStorageService _localStorageService;
-        public AuthService(ILocalStorageService localStorageService)
+        private HttpClient _httpClient;
+
+
+        private IConfiguration _configuration;  
+        public AuthService(ILocalStorageService localStorageService, HttpClient httpClient, IConfiguration configuration)
         {
             _localStorageService = localStorageService;
+            _httpClient = httpClient;
+            _configuration = configuration;
         }
 
         public async Task<bool> IsSignedIn()
@@ -68,5 +83,42 @@ namespace frontend.Services
                 return userData;
             }
         }
-    } 
+        public async Task<int> GetRole(string Token)
+        {
+            string strapi_api_url;
+
+            strapi_api_url = _configuration["AppSettings:STRAPI_API_URL"];
+            var url = "{STRAPI_API_URL}/api/users/me?populate=role";
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", Token);
+            var response = await _httpClient.GetAsync(url.Replace("{STRAPI_API_URL}", strapi_api_url));
+            var userData = await response.Content.ReadAsStringAsync();
+            var roleData = JsonSerializer.Deserialize<Me>(userData) ?? throw new Exception("Login Response is null");
+
+            return roleData.role.id;
+        }
+
+        public async Task<bool> IsElevated(int role)
+        {
+            if(role == 3)
+            {
+                return true;
+            }
+            return false;
+        }
+    }   
+
+
+    public class Me
+    {
+        public int id { get; set; }
+        public Role role { get; set; } 
+    }
+
+    public class Role
+    {
+        public int id { get; set; } 
+        public string name { get; set; }
+        public string type { get; set; }
+    }   
 }
